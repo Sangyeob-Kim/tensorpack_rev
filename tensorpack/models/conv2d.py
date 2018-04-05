bc42ba11a13c7d6077276a72b3342db8936d4546
Sangyeob-Kim <37903179+Sangyeob-Kim@users.noreply.github.com>
2018-04-05 17:19:08 +0900

    Update conv2d.py

--- tensorpack/models/conv2d.py
+++ tensorpack/models/conv2d.py
@@ -148,37 +148,42 @@ def Conv2D(
                     k = tf.add(b,c)
                 
                 outputs = tf.nn.conv2d(i, tf.transpose(k, perm=[0,1,3,2]), stride, padding.upper(), **kwargs)
-                #b = tf.add(outputs,-1*tf.mod(outputs,(tf.div(outputs,outputs) * tmp2)))
-                #c = tf.floor(tf.div(outputs,tmp3))
-                #c = tf.round((tf.div(c,c+0.1)))
-                #c = tf.add(c*tmp,c*b*-1)
-                #outputs = tf.add(b,c)
+                
+		with tf.device('/cpu:0'):
+		    b = tf.add(outputs,-1*tf.mod(outputs,(tf.div(outputs,outputs) * tmp2)))
+                    c = tf.floor(tf.div(outputs,tmp3))
+                    c = tf.round((tf.div(c,c+0.1)))
+                    c = tf.add(c*tmp,c*b*-1)
+                    outputs = tf.add(b,c)
 		
             else:
-                #b = tf.add(i,-1*tf.mod(i,(tf.div(i,i) * tmp2)))
-                #c = tf.floor(tf.div(i,tmp3))
-                #c = tf.round((tf.div(c,c+0.1)))
-                #c = tf.add(c*tmp,c*b*-1)
-                #i = tf.add(b,c)
+		with tf.device('/cpu:0'):
+                    b = tf.add(i,-1*tf.mod(i,(tf.div(i,i) * tmp2)))
+                    c = tf.floor(tf.div(i,tmp3))
+                    c = tf.round((tf.div(c,c+0.1)))
+                    c = tf.add(c*tmp,c*b*-1)
+                    i = tf.add(b,c)
                
-                #b = tf.add(k,-1*tf.mod(k,(tf.div(k,k) * tmp2)))
-                #c = tf.floor(tf.div(k,tmp3))
-                #c = tf.round((tf.div(c,c+0.1)))
-                #c = tf.add(c*tmp,c*b*-1)
-                #k = tf.add(b,c)
+                    b = tf.add(k,-1*tf.mod(k,(tf.div(k,k) * tmp2)))
+                    c = tf.floor(tf.div(k,tmp3))
+                    c = tf.round((tf.div(c,c+0.1)))
+                    c = tf.add(c*tmp,c*b*-1)
+                    k = tf.add(b,c)
                 
                 outputs2 = tf.nn.conv2d(i, tf.transpose(k, perm=[0,1,3,2]), stride, padding.upper(), **kwargs)
-                #b = tf.add(outputs2,-1*tf.mod(outputs2,(tf.div(outputs2,outputs2) * tmp2)))
-                #c = tf.floor(tf.div(outputs2,tmp3))
-                #c = tf.round((tf.div(c,c+0.1)))
-                #c = tf.add(c*tmp,c*b*-1)
-                #outputs2 = tf.add(b,c)
+                with tf.device('/cpu:0'):
+		    b = tf.add(outputs2,-1*tf.mod(outputs2,(tf.div(outputs2,outputs2) * tmp2)))
+                    c = tf.floor(tf.div(outputs2,tmp3))
+                    c = tf.round((tf.div(c,c+0.1)))
+                    c = tf.add(c*tmp,c*b*-1)
+                outputs2 = tf.add(b,c)
                 outputs = tf.add(outputs, outputs2)
-                #b = tf.add(outputs,-1*tf.mod(outputs,(tf.div(outputs,outputs) * tmp2)))
-                #c = tf.floor(tf.div(outputs,tmp3))
-                #c = tf.round((tf.div(c,c+0.1)))
-                #c = tf.add(c*tmp,c*b*-1)
-                #outputs = tf.add(b,c)
+                with tf.device('/cpu:0'):
+	            b = tf.add(outputs,-1*tf.mod(outputs,(tf.div(outputs,outputs) * tmp2)))
+                    c = tf.floor(tf.div(outputs,tmp3))
+                    c = tf.round((tf.div(c,c+0.1)))
+                    c = tf.add(c*tmp,c*b*-1)
+                    outputs = tf.add(b,c)
             count+=1
             
         #print("\noutputs")
