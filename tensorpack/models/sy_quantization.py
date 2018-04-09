0e75e1441cb152d5e93f1944800cedb651c5b5d9
Sangyeob-Kim <37903179+Sangyeob-Kim@users.noreply.github.com>
2018-04-10 02:37:44 +0900

    Update sy_quantization.py

--- tensorpack/models/sy_quantization.py
+++ tensorpack/models/sy_quantization.py
@@ -102,6 +102,23 @@ def sy_quantization(
     #if use_bias:
     #    ret.variables.b = layer.bias
     #return tf.identity(ret, name='output')
+    before = 32
+    tmp = 0.0 
+    after_div2=after/2
+	
+    for i in range(after-1):
+        if((i-after_div2)<0):
+            tmp+= 1.0/np.power(2,-i+after_div2)
+        else:
+            tmp += np.power(2,i-after_div2)
+
+    max_range = tmp
+    min_range = -1.0*np.power(2,after_div2-1) 
+    range_T = np.power(2,after-1) * 2.0 - 1.0
+    range_T_add_1_div_2 = (range_T + 1.0)/2.0
+    range_target = max_range - min_range
+    range_div_range_T = range_target/range_T
+    one_over_range_div_range_T =1/ range_div_range_T
     inputs = tf.round((inputs - min_range) * (one_over_range_div_range_T) - range_T_add_1_div_2)
     inputs = (inputs+range_T_add_1_div_2)
     inputs = min_range+(inputs*range_div_range_T)
