37b7522bd6b761bf3949ecf566fb8ff6aa787698
Sangyeob-Kim <37903179+Sangyeob-Kim@users.noreply.github.com>
2018-04-14 11:39:00 +0900

    Update conv2d.py

--- tensorpack/models/conv2d.py
+++ tensorpack/models/conv2d.py
@@ -259,8 +259,8 @@ def Conv2D(
                     #k = min_range+(k*range_div_range_T)
                     #k = tf.clip_by_value(k,min_range,max_range)
 
-                with G.gradient_override_map({"Identity" : "CustomGrad_for_conv_"+str(after)+"bit"}):
-                    outputs = tf.identity(outputs)
+                #with G.gradient_override_map({"Identity" : "CustomGrad_for_conv_"+str(after)+"bit"}):
+                #    outputs = tf.identity(outputs)
 		
                 outputs = tf.nn.conv2d(i, tf.transpose(k, perm=[0,1,3,2]), stride, padding.upper(), **kwargs)
 
@@ -304,8 +304,8 @@ def Conv2D(
                     #k = (k+range_T_add_1_div_2)
                     #k = min_range+(k*range_div_range_T)
                     #k = tf.clip_by_value(k,min_range,max_range)
-                with G.gradient_override_map({"Identity" : "CustomGrad_for_conv_"+str(after)+"bit"}):
-                    outputs2 = tf.identity(outputs2)
+                #with G.gradient_override_map({"Identity" : "CustomGrad_for_conv_"+str(after)+"bit"}):
+                #    outputs2 = tf.identity(outputs2)
 		
                 outputs2 = tf.nn.conv2d(i, tf.transpose(k, perm=[0,1,3,2]), stride, padding.upper(), **kwargs)
 #                if (after != 0):
@@ -339,8 +339,8 @@ def Conv2D(
                     #outputs2 = outputs2 * (1.0/np.power(2,after-2))
                     #outputs2 = tf.clip_by_value(outputs2,1.0/np.power(2,after-2),tmp)
                     #outputs2 = outputs2*y
-                with G.gradient_override_map({"Identity" : "CustomGrad_for_conv_"+str(after)+"bit"}):
-                    outputs = tf.identity(outputs)
+                #with G.gradient_override_map({"Identity" : "CustomGrad_for_conv_"+str(after)+"bit"}):
+                #    outputs = tf.identity(outputs)
 		
                 outputs = tf.add(outputs, outputs2)
 #                if (after != 0):
