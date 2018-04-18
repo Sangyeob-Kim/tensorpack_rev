db11a7f76c7e985b472994d399ac3830976a902a
Sangyeob-Kim <37903179+Sangyeob-Kim@users.noreply.github.com>
2018-04-18 15:31:08 +0900

    Update conv2d.py

--- tensorpack/models/conv2d.py
+++ tensorpack/models/conv2d.py
@@ -265,8 +265,8 @@ def Conv2D(
             tf.summary.histogram(inputs.name, inputs)
             y = tf.sign(inputs)
             inputs = tf.abs(inputs)
-            inputs = tf.floor(inputs / ((min)*np.power(2,10)))
-            inputs = inputs * ((min)*np.power(2,10))
+            inputs = tf.floor(inputs / min)
+            inputs = inputs * (min)
             inputs = tf.clip_by_value(inputs,min,tmp)
             inputs = inputs*y
             inputs = tf.Print(inputs,[inputs[0]])
